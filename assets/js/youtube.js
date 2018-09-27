89aa99fe261fc7de867adb50664ad74fdac26f75
Oleg Sidorkin <oleg.sidorkin@gmail.com>
2018-09-26 18:08:14 -0700

    Create youtube.js

---
+++ assets/js/youtube.js
@@ -0,0 +1,27 @@
+$(function(){
+  $("body").find("a").each(function(n,a) {
+    let match = a.href.match(/https?:\/\/www\.youtube\.com\/watch\?v=(.*)$/)
+    if (match) {
+      let video = match[1]
+      let wrapper = document.createElement("div")
+      wrapper.style.position = "relative"
+      wrapper.style.height = "0"
+      wrapper.style.paddingTop = "25px"
+      wrapper.style.paddingBottom =  "56.25%"
+
+      let iframe = document.createElement("iframe")
+      iframe.src = "https://www.youtube-nocookie.com/embed/#{video}"
+      iframe.style.border = "none"
+      iframe.style.position = "absolute"
+      iframe.style.top = "0"
+      iframe.style.left = "0"
+      iframe.style.width = "100%"
+      iframe.style.height = "100%"
+      iframe.allow = "autoplay; encrypted-media"
+      iframe.setAttribute("allowFullScreen", "")
+
+      wrapper.appendChild(iframe)
+      a.parentNode.insertBefore(wrapper, a)
+      a.remove()
+  })
+})
