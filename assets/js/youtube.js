717f5af2e6bea8f769e7c0872ace0a3e137bbdf2
Oleg Sidorkin <oleg.sidorkin@gmail.com>
2018-09-26 18:40:56 -0700

    Update youtube.js

--- assets/js/youtube.js
+++ assets/js/youtube.js
@@ -10,7 +10,7 @@
       wrapper.style.paddingBottom =  "56.25%"
       
       let iframe = document.createElement("iframe")
-      iframe.src = "https://www.youtube-nocookie.com/embed/" + video
+      iframe.src = "https://www.youtube-nocookie.com/embed/" + video + "?rel=0"
       iframe.style.border = "none"
       iframe.style.position = "absolute"
       iframe.style.top = "0"
